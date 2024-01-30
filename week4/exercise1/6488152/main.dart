307317ce9f30c62e8b777cf6f87c7f6ccee8609a
parewawa22 <127741730+parewawa22@users.noreply.github.com>
2024-01-30 11:42:45 +0700

    main_6488152.dart

---
+++ week4/exercise1/6488152/main.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //set structure of our app
+      title: '6488152',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Product layout demo home page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("6488152 Product Listing")),
+      body: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.fromLTRB(2.0, 10.0, 2.0, 10.0),
+        children: <Widget>[
+          ProductBox(
+            name: "iPhone",
+            description: "iPhone is the stylist phone ever",
+            price: 1000,
+            image: "iphone.jpg",
+          ),
+          ProductBox(
+            name: "Pixel",
+            description: "Pixel is the most featureful phone ever",
+            price: 800,
+            image: "pixel.jpg",
+          ),
+          ProductBox(
+            name: "Laptop",
+            description: "Laptop is the most productive development tool",
+            price: 2000,
+            image: "laptop.jpg",
+          ),
+          ProductBox(
+            name: "Tablet",
+            description: "Tablet is the most useful device ever for meeting",
+            price: 1500,
+            image: "tablet.jpg",
+          ),
+          ProductBox(
+            name: "Pendrive",
+            description: "Pendrive is a useful storage medium",
+            price: 100,
+            image: "pendrive.jpg",
+          ),
+          ProductBox(
+            name: "Floppy Drive",
+            description: "Floppy drive is a useful rescue storage medium",
+            price: 20,
+            image: "floppy_drive.png",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductBox extends StatelessWidget {
+  ProductBox(
+      {Key? key,
+      required this.name,
+      required this.description,
+      required this.price,
+      required this.image})
+      : super(key: key);
+  final String name;
+  final String description;
+  final int price;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(2),
+      height: 120,
+      child: Card(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Image.asset("assets/appimages/" + image),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(5),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text(this.name,
+                        style: TextStyle(fontWeight: FontWeight.bold)),
+                    Text(this.description),
+                    Text("Price: " + this.price.toString()),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
