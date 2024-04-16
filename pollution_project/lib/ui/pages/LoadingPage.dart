5b9c8c7e46b516a73ed8598d266bd241b202ccc0
Warittha Tarntong <75978876+waritthatarntong@users.noreply.github.com>
2024-04-16 18:04:20 +0700

    Edit LoadingPage

--- pollution_project/lib/ui/pages/LoadingPage.dart
+++ pollution_project/lib/ui/pages/LoadingPage.dart
@@ -6,9 +6,7 @@ class LoadingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-      ), // Show a spinning loader
+      child: CircularProgressIndicator(), // Show a spinning loader
     );
   }
 }
